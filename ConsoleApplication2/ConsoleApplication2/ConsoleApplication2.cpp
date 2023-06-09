55179ea6980ed668ac9eedd7c82a802a72457955
yeah im the rEal 6or! <106602449+x6or@users.noreply.github.com>
2023-06-09 12:37:37 -0700

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <thread>
+#include <chrono>
+
+using namespace std;
+
+void workTimer(int minutes) {
+    cout << "Starting work for " << minutes << " minutes." << endl;
+    this_thread::sleep_for(chrono::minutes(minutes));
+    cout << "Work session completed!" << endl;
+}
+
+void breakTimer(int minutes) {
+    cout << "Taking a break for " << minutes << " minutes." << endl;
+    this_thread::sleep_for(chrono::minutes(minutes));
+    cout << "Break is over!" << endl;
+}
+
+int main() {
+    int workTime; 
+    int breakTime;
+    cout << "enter the work time : " << endl;
+    cin >> workTime;
+        cout << "enter the break time : " << endl;
+        cin >> breakTime;
+
+    cout << "Pomodoro Technique" << endl;
+
+    while (true) {
+        workTimer(workTime);
+        breakTimer(breakTime);
+    }
+
+    return 0;
+}
